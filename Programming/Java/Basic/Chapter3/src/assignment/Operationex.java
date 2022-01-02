32c025ee7534c2c461ff648f07b0213e242d695b
netager <thinkjbank@gmail.com>
2022-01-02 23:32:46 +0900

    20220102 Home Review Java Programming

---
+++ Programming/Java/Basic/Chapter3/src/assignment/Operationex.java
@@ -0,0 +1,10 @@
+package assignment;
+
+public class Operationex {
+
+	public static void main(String[] args) {
+		int age = 24;
+		System.out.println(age);
+	}
+
+}
---
+++ Programming/Java/workspace/Chapter7/src/array/ArrayListTest.java
@@ -0,0 +1,19 @@
+package array;
+
+import java.util.*;
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("aaa");
+		list.add("bbb");
+		list.add("ccc");
+		
+		for(int i=0; i<list.size(); i++) {
+			String s = list.get(i);
+			System.out.println(s);
+		}
+	}
+
+}
