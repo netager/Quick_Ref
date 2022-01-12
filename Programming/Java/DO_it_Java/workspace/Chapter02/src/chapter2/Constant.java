b9c2e9d672276015d4154c4115c767d87e51745a
netager <thinkjbank@gmail.com>
2022-01-12 18:14:45 +0900

    20220112 Office Review Java Programming Basic

---
+++ Programming/Java/DO_it_Java/workspace/Chapter02/src/chapter2/Constant.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class Constant {
+
+	public static void main(String[] args) {
+
+		final int MAX_NUM = 100; // 상수는 대분자로 관습적으로 사용함
+		final float PI = 3.14f;
+		
+		final int STUDENT_NUM = 30;
+		
+		int num = 0;
+		if (num == STUDENT_NUM) {}
+		
+		System.out.println(STUDENT_NUM);
+		
+	}
+
+}
