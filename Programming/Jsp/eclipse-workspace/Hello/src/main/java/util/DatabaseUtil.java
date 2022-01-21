3e8c8b484d4dfb46417b373bba2617ba7cf52462
netager <thinkjbank@gmail.com>
2022-01-21 18:10:21 +0900

    20220121 Office Review Jsp Programming Basic

---
+++ Programming/Jsp/eclipse-workspace/Hello/src/main/java/util/DatabaseUtil.java
@@ -0,0 +1,23 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseUtil {
+	
+	public static Connection getConnection() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/TUTORIAL?characterEncoding=UTF-8&serverTimezone=UTC";
+			String dbID = "root";
+			String dbPassword = "tnscjs1%";
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			return DriverManager.getConnection(dbURL, dbID, dbPassword);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+
+}
