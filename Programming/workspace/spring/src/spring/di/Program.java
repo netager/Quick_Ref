df1a46fdd7dc8df52cc53b68ccac07f07c4343d3
netager <thinkjbank@gmail.com>
2022-04-25 22:11:43 +0900

    20220425 Home Review Spring

--- Programming/workspace/spring/src/spring/di/Program.java
+++ Programming/workspace/spring/src/spring/di/Program.java
@@ -36,19 +36,19 @@ public class Program {
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("spring/di/setting.xml");
 				
-		Exam exam = context.getBean(Exam.class);
-			System.out.println(exam.toString());
+//		Exam exam = context.getBean(Exam.class);
+//			System.out.println(exam.toString());
 		
 		ExamConsole console = (ExamConsole) context.getBean("console");
 //		ExamConsole console = context.getBean(ExamConsole.class);  // 자료형명을 이용하여 사용
 		console.print();
 		
 //		List<Exam> exams = new ArrayList<>();
-		List<Exam> exams = (List<Exam>) context.getBean("exams");
+//		List<Exam> exams = (List<Exam>) context.getBean("exams");
 //		exams.add(new NewlecExam(1,1,1,1));
 		
-		for(Exam e : exams)
-			System.out.println(e);
+//		for(Exam e : exams)
+//			System.out.println(e);
 
 	}
 
--- Programming/workspace/spring/src/spring/di/ui/InlineExamConsole.java
+++ Programming/workspace/spring/src/spring/di/ui/InlineExamConsole.java
@@ -1,20 +1,25 @@
 package spring.di.ui;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
 import spring.di.entity.Exam;
 
+
 public class InlineExamConsole implements ExamConsole {
-	
+	@Autowired(required = false)
+	@Qualifier("exam2")
 	private Exam exam;
 
 
 	public InlineExamConsole() {
-		// TODO Auto-generated constructor stub
+		System.out.println("constructor");
 	}
-	
+
+
 	public void InlineConsole(Exam exam) {
-	
+		System.out.println("overloaded constructor");
 		this.exam = exam;
 	}
 	
@@ -22,16 +27,17 @@ public class InlineExamConsole implements ExamConsole {
 	@Override
 	public void print() {
 		
-		
-		System.out.printf("total : %3d, avg : %3.2f\n", exam.total(), exam.avg());
+		if(exam == null)
+			System.out.printf("total : %3d, avg : %3.2f\n", 0, 0.0);
+		else
+			System.out.printf("total : %3d, avg : %3.2f\n", exam.total(), exam.avg());
 	
 	}
 	
-	
-	@Autowired
+
 	@Override
 	public void setExam(Exam exam) {
-		// TODO Auto-generated method stub
+		System.out.println("setter");
 		this.exam = exam;
 	}	
 }
