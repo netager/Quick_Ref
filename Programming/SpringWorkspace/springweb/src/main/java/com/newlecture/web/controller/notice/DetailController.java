195d66590b85fc83e09c54206993495496f76f39
netager <thinkjbank@gmail.com>
2022-04-30 04:25:44 +0900

    20220429 Home Review Spring MVC

---
+++ Programming/SpringWorkspace/springweb/src/main/java/com/newlecture/web/controller/notice/DetailController.java
@@ -0,0 +1,20 @@
+package com.newlecture.web.controller.notice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class DetailController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		System.out.println("[DetailController] Detail Controller");
+		ModelAndView mv = new ModelAndView("notice.detail"); 
+
+		return mv;
+	}
+
+}
