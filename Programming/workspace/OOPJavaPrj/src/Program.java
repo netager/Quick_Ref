32d5d7a702d5322602f0a02c0bbe59599059a5a4
netager <thinkjbank@gmail.com>
2022-05-09 23:24:07 +0900

    20220509 Office Review Java Programming

--- Programming/workspace/OOPJavaPrj/src/Program.java
+++ Programming/workspace/OOPJavaPrj/src/Program.java
@@ -10,7 +10,12 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Exam exam = new Exam(1,1,1);
+		NewlecExam exam = new NewlecExam(1,1,1,1);
+		exam.setKor(10);
+		exam.setEng(10);
+		exam.setMath(10);
+		exam.setCom(10);
+		
 		System.out.println(exam.total());
 		System.out.println(exam.avg());
 
