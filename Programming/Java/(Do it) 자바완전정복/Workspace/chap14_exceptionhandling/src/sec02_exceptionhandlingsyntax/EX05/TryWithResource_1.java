1e91c2a2ccb799eff5f976ad0ef559033ef89fef
netager <thinkjbank@gmail.com>
2022-05-24 23:12:12 +0900

    20220524 Home Review Java Programming

--- Programming/Java/(Do it) 자바완전정복/Workspace/chap14_exceptionhandling/src/sec02_exceptionhandlingsyntax/EX05/TryWithResource_1.java
+++ Programming/Java/(Do it) 자바완전정복/Workspace/chap14_exceptionhandling/src/sec02_exceptionhandlingsyntax/EX05/TryWithResource_1.java
@@ -1,5 +1,46 @@
 package sec02_exceptionhandlingsyntax.EX05;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class TryWithResource_1 {
+	public static void main(String[] args) {
+		
+		//참고. System.in 리소스를 해제하면 더이상 콘솔입력이 불가
+		//# 자동 리소스 해제
+		try(InputStreamReader isr1 = new InputStreamReader(System.in);) {
+			char input = (char)isr1.read();
+			System.out.println("입력글자 = " + input);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			
+		}
+		
+		//#2. 수동 리소스 해제
+		InputStreamReader isr2 = null;
+		try {
+			isr2 = new InputStreamReader(System.in);
+			char input = (char)isr2.read();
+			System.out.println("입력글자 = " + input);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			
+		}
+		finally {
+			//리소스 해제 구문
+			if(isr2 != null) {
+				try {
+					isr2.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} // 리소스 해제
+			}
+		}
+		
+	}
+	
 
 }
---
+++ Programming/Java/(Do it) 자바완전정복/Workspace/chap14_exceptionhandling/src/sec03_throwsexception/EX01/ThrowsException_1.java
@@ -0,0 +1,12 @@
+package sec03_throwsexception.EX01;
+
+//#1. 하위메서드에 직접 예외를 처리하는 방법
+class A {
+	
+	void bcd() {
+		Thread.sleep(1000));
+	}
+}
+public class ThrowsException_1 {
+
+}
