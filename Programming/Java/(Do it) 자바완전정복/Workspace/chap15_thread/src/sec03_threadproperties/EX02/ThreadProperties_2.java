632788ffd4faa1c6173e6c790e0eb473c09b2732
netager <thinkjbank@gmail.com>
2022-05-26 20:03:13 +0900

    20220526 Home Review Java Programming

---
+++ Programming/Java/(Do it) 자바완전정복/Workspace/chap15_thread/src/sec03_threadproperties/EX02/ThreadProperties_2.java
@@ -0,0 +1,19 @@
+package sec03_threadproperties.EX02;
+
+//우선순위
+
+class MyThread extends Thread {
+	@Override
+	public void run() {
+		for (long i=0; i<1000000000)
+	}
+}
+public class ThreadProperties_2 {
+	public static void main(String[] args) {
+		
+		//#참고. CPU Core
+		System.out.println("코어수 : " + Runtime.getRuntime().availableProcessors());
+		
+		//#1. 디폴트 우선순위
+	}
+}
