180091b79c2662a9dfc9948fd81fbb3c568e2e4a
netager <thinkjbank@gmail.com>
2024-01-10 22:36:57 +0900

    20240110 Home Review Spring

--- Programming/SpringBoot/office_sbb/sbb/src/main/java/com/mysite/sbb/user/UserService.java
+++ Programming/SpringBoot/office_sbb/sbb/src/main/java/com/mysite/sbb/user/UserService.java
@@ -1,8 +1,11 @@
 package com.mysite.sbb.user;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import java.util.Optional;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.mysite.sbb.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,6 +25,14 @@ public class UserService {
 		return user;
 	}
 
+	public SiteUser getUser(String username) {
+		Optional<SiteUser> siteUser = this.userRepository.findByUsername(username);
+		if(siteUser.isPresent()) {
+			return siteUser.get();
+		}else {
+			throw new DataNotFoundException("siteuser not found");
+		}
+	}
 	
 //	public List<Question> getList() {
 //		return this.questionRepository.findAll();
